501e40aedaa56fd6fd01fd89b8eea9d79f5bf751
JS Soluções e Serviços <49538838+jssolucoes-e-servicos@users.noreply.github.com>
2025-11-20 00:30:53 -0300

    change cors

--- src/common/configs/cors.config.ts
+++ src/common/configs/cors.config.ts
@@ -3,6 +3,7 @@ export const corsOrigns = [
   'http://localhost:3002',
   'http://localhost:3004',
   'http://192.168.0.104:3004',
+  'http://192.168.1.67:3004',
   'https://dogao.igrejavivaemcelulas.com.br',
   'https://erp.dogao.igrejavivaemcelulas.com.br',
   'https://entregas-dogao.igrejavivaemcelulas.com.br',
