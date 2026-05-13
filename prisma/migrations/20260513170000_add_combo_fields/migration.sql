a7743182f0d4e064437d8e142fa35aae3135d7fa
JS Soluções e Serviços <49538838+jssolucoes-e-servicos@users.noreply.github.com>
2026-05-13 18:38:54 -0300

    fix migrations

--- prisma/migrations/20260513170000_add_combo_fields/migration.sql
+++ prisma/migrations/20260513170000_add_combo_fields/migration.sql
@@ -3,3 +3,7 @@ ALTER TABLE "editions" ADD COLUMN "comboActive" BOOLEAN NOT NULL DEFAULT false;
 
 -- AlterTable
 ALTER TABLE "orders_items" ADD COLUMN "isPromo" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "customers_addresses" ADD COLUMN "lat" DOUBLE PRECISION;
+ALTER TABLE "customers_addresses" ADD COLUMN "lng" DOUBLE PRECISION;
